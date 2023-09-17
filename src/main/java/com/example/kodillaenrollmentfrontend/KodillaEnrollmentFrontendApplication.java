ce2a1613e3784047253c051a4bc76522cf68a370
Kasia Gierasimczuk <k.gierasimczuk@gmail.com>
2023-09-18 00:03:07 +0200

    first commit

---
+++ src/main/java/com/example/kodillaenrollmentfrontend/KodillaEnrollmentFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.example.kodillaenrollmentfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaEnrollmentFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaEnrollmentFrontendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kodillaenrollmentfrontend/KodillaEnrollmentFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kodillaenrollmentfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaEnrollmentFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
